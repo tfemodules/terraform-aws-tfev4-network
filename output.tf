2401cb6aeed28f2cfd9f7411ee7f12b6b822ce29
Slav Donchev <slav.donchev@gmail.com>
2020-10-28 15:53:16 +0200

    refacrot output

--- output.tf
+++ output.tf
@@ -4,13 +4,13 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value       = { for k, v in aws_subnet.public : k => v.id }
+  value       = values(aws_subnet.public)[*].id
   description = "Ids of the 'public' subents."
 }
 
-output "public_subnets" {
-  value       = aws_subnet.public
-  description = "A mapping of input public subnet CIDR and created subnet."
+output "public_subnet_cidr_to_id" {
+  value       = { for cidr, id in aws_subnet.public : cidr => id }
+  description = "A mapping of the public subent CIRDs to their Ids."
 }
 
 output "private_subnet_ids" {
@@ -18,9 +18,9 @@ output "private_subnet_ids" {
   description = "Ids of the private subents."
 }
 
-output "private_subnets" {
-  value       = aws_subnet.private
-  description = "A mapping of input private subnet CIDR and created subnet."
+output "private_subnet_cidr_to_id" {
+  value       = { for cidr, id in aws_subnet.private : cidr => id }
+  description = "A mapping of the private subent CIRDs to their Ids."
 }
 
 output "rds_subnet_ids" {
@@ -28,24 +28,9 @@ output "rds_subnet_ids" {
   description = "Ids of the private subents."
 }
 
-output "rds_subnets" {
-  value       = aws_subnet.rds
-  description = "A mapping of input private subnet CIDR and created subnet."
-}
-
-output "main_route_table_id" {
-  value       = aws_vpc.main.main_route_table_id
-  description = "The id of the VPC default routing table. Used by the private subnets"
-}
-
-output "private_route_table_id" {
-  value       = aws_route_table.private.id
-  description = "The id of the private subnets route table."
-}
-
-output "public_route_table_id" {
-  value       = aws_route_table.public.id
-  description = "The id of the public subnets route table."
+output "rds_subnet_cidr_to_id" {
+  value       = { for cidr, id in aws_subnet.rds : cidr => id }
+  description = "A mapping of the RDS subent CIRDs to their Ids."
 }
 
 output "nat_gateway_public_ip" {
