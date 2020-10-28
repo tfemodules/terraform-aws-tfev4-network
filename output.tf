1b3123fb2a47a1280c874ed540495c8e5308d4e0
Slav Donchev <slav.donchev@gmail.com>
2020-10-28 21:20:23 +0200

    fix spelling mistakes

--- output.tf
+++ output.tf
@@ -5,32 +5,32 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   value       = values(aws_subnet.public)[*].id
-  description = "Ids of the 'public' subents."
+  description = "Ids of the 'public' subnets."
 }
 
 output "public_subnet_cidr_to_id" {
   value       = { for cidr, id in aws_subnet.public : cidr => id }
-  description = "A mapping of the public subent CIRDs to their Ids."
+  description = "A mapping of the public subnets CIDRs to their Ids."
 }
 
 output "private_subnet_ids" {
   value       = values(aws_subnet.private)[*].id
-  description = "Ids of the private subents."
+  description = "Ids of the private subnets."
 }
 
 output "private_subnet_cidr_to_id" {
   value       = { for cidr, id in aws_subnet.private : cidr => id }
-  description = "A mapping of the private subent CIRDs to their Ids."
+  description = "A mapping of the private subnets CIDRs to their Ids."
 }
 
 output "rds_subnet_ids" {
   value       = values(aws_subnet.rds)[*].id
-  description = "Ids of the private subents."
+  description = "Ids of the private subnets."
 }
 
 output "rds_subnet_cidr_to_id" {
   value       = { for cidr, id in aws_subnet.rds : cidr => id }
-  description = "A mapping of the RDS subent CIRDs to their Ids."
+  description = "A mapping of the RDS subnets CIDRs to their Ids."
 }
 
 output "nat_gateway_public_ip" {
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "vpc_cidr_block" {
 
 variable "availability_zones" {
   type        = list(string)
-  description = "A list of two AWS availabilty zones in which to deploy the infrastructure. If not provided Terraform will use any two zones in the current region."
+  description = "A list of two AWS availability zones in which to deploy the infrastructure. If not provided Terraform will use any two zones in the current region."
   default     = []
   validation {
     condition     = length(var.availability_zones) == 0 || length(var.availability_zones) == 2
@@ -15,7 +15,7 @@ variable "availability_zones" {
 
 variable "public_subnets_cidrs" {
   type        = list(string)
-  description = "A list of exactly two CIDRs for the public subents. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
+  description = "A list of exactly two CIDRs for the public subnets. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
   validation {
     condition     = length(var.public_subnets_cidrs) == 2
     error_message = "The 'public_subnets_cidrs' variable value must be a list with exactly 2 elements."
@@ -24,7 +24,7 @@ variable "public_subnets_cidrs" {
 
 variable "private_subnets_cidrs" {
   type        = list(string)
-  description = "A list of exactly two CIDRs for the private subents. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
+  description = "A list of exactly two CIDRs for the private subnets. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
   validation {
     condition     = length(var.private_subnets_cidrs) == 2
     error_message = "The 'private_subnets_cidrs' variable value must be a list with exactly 2 elements."
@@ -33,7 +33,8 @@ variable "private_subnets_cidrs" {
 
 variable "rds_subnets_cidrs" {
   type        = list(string)
-  description = "A list of 0 or two CIDRs for the RDS subent group. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
+  description = "A list of 0 or two CIDRs for the RDS subnet group. The CIDRs must be within the CIDR set for 'vpc_cidr_block'."
+  default = []
   validation {
     condition     = length(var.rds_subnets_cidrs) == 0 || length(var.rds_subnets_cidrs) == 2
     error_message = "The 'rds_subnets_cidrs' variable value must be a list with 0 or 2 elements."
@@ -42,12 +43,12 @@ variable "rds_subnets_cidrs" {
 
 variable "name_prefix" {
   type        = string
-  description = "A string to be used as prefix for generating names of the created resources"
+  description = "A string to be used as prefix for generating names of the created resources."
   default     = "tfe-"
 }
 
 variable "common_tags" {
   type        = map(string)
-  description = "Common tags to assign to all resources"
+  description = "Common tags to assign to all resources."
   default     = {}
 }
